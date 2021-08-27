4b5b087ca4dd7a945ddb71486ba935e0a809433d
Pablo Flores <pabloguillermoflores93@gmail.com>
2021-08-27 14:06:38 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,48 @@
+import { DashboardModule } from './@admin/pages/dashboard/dashboard.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ //Configuracion de rutas
+
+{
+   path: 'home', 
+   loadChildren: ( )=> import('./@public/pages/home/home.module').then(m =>m.HomeModule)
+},
+{
+  path: 'contact', 
+  loadChildren: ( )=> import('./@public/pages/contact/contact.module').then(m =>m.ContactModule)
+},
+{
+  path: 'admin', 
+  loadChildren: ( )=> import('./@admin/pages/dashboard/dashboard.module').then(m =>m.DashboardModule)
+},
+{
+  path: 'admin/users', 
+  loadChildren: ( )=> import('./@admin/pages/users/users.module').then(m =>m.UsersModule)
+},
+{
+  path: '', 
+  redirectTo: 'home',
+  pathMatch:'full'
+}, 
+
+{
+  path: '**', 
+  redirectTo: 'home',
+  pathMatch:'full'
+}  
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      scrollPositionRestoration: 'enabled'
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
